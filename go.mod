8a6d8a1ba1805498b0d000c75580c70978da1c38
Sören Michaelis <soeren.michaelis11@gmail.com>
2025-01-20 22:28:40 +0100

    added simple http healthcheck

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Soemii/distroless-healthcheck
 
 go 1.23
+
+require github.com/urfave/cli/v3 v3.0.0-beta1
--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "log"
+import (
+	"context"
+	"fmt"
+	"github.com/urfave/cli/v3"
+	"net/http"
+	"strings"
+
+	"os"
+)
+
+func httpCommand() *cli.Command {
+	return &cli.Command{
+		Name:  "http",
+		Usage: "checks the health of an http service",
+		Flags: []cli.Flag{&cli.UintFlag{
+			Name:    "status",
+			Value:   200,
+			Aliases: []string{"c"},
+		}},
+		Action: func(ctx context.Context, command *cli.Command) error {
+			url := command.Args().First()
+			if strings.Trim(url, " ") == "" {
+				return cli.Exit("url is required", 1)
+			}
+			get, err := http.Get(url)
+			if err != nil {
+				return cli.Exit(err.Error(), 2)
+			}
+			statusCode := command.Uint("status")
+			if get.StatusCode != int(statusCode) {
+				return cli.Exit(fmt.Sprintf("expected status code %d but got %d", statusCode, get.StatusCode), 1)
+			}
+			return nil
+		},
+	}
+}
 
 func main() {
-	log.Println("Hello, World!")
+	err := (&cli.Command{
+		Name:     "healthcheck",
+		Usage:    "cli tool to check health of services",
+		Commands: []*cli.Command{httpCommand()},
+	}).Run(context.Background(), os.Args)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
