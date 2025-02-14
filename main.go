13d8b3fa9fc653c24af08777be232adf1393caa1
Sören Michaelis <soeren.michaelis11@gmail.com>
2025-02-14 22:04:12 +0100

    added tcp support

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"github.com/urfave/cli/v3"
+	"net"
 	"net/http"
 	"strings"
+	"time"
 
 	"os"
 )
@@ -37,11 +39,37 @@ func httpCommand() *cli.Command {
 	}
 }
 
+func tcpCommand() *cli.Command {
+	return &cli.Command{
+		Name:  "tcp",
+		Usage: "checks the health of a tcp service",
+		Flags: []cli.Flag{&cli.UintFlag{
+			Name:    "timeout",
+			Value:   500,
+			Aliases: []string{"t"},
+		}},
+		Action: func(ctx context.Context, command *cli.Command) error {
+			url := command.Args().First()
+			if strings.Trim(url, " ") == "" {
+				return cli.Exit("uri is required", 1)
+			}
+			dial, err := net.DialTimeout("tcp", url, time.Millisecond*time.Duration(command.Uint("timeout")))
+			if err != nil {
+				return err
+			}
+			if dial != nil {
+				defer dial.Close()
+			}
+			return nil
+		},
+	}
+}
+
 func main() {
 	err := (&cli.Command{
 		Name:     "healthcheck",
 		Usage:    "cli tool to check health of services",
-		Commands: []*cli.Command{httpCommand()},
+		Commands: []*cli.Command{httpCommand(), tcpCommand()},
 	}).Run(context.Background(), os.Args)
 	if err != nil {
 		fmt.Println(err)
